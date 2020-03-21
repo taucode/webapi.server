b206bd4e37567f5c099225cc7cdd35e046e9c805
Andrey Kovalenko <ak@taucode.com>
2020-03-21 18:05:22 +0200

    Updated.

--- tests/TauCode.WebApi.Server.Tests/TestFactory.cs
+++ tests/TauCode.WebApi.Server.Tests/TestFactory.cs
@@ -3,21 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
 using TauCode.WebApi.Server.Tests.AppHost;
-using MicrosoftHost = Microsoft.Extensions.Hosting.Host;
 
 namespace TauCode.WebApi.Server.Tests
 {
     public class TestFactory : WebApplicationFactory<Startup>
     {
-        //protected override IWebHostBuilder CreateWebHostBuilder()
-        //{
-        //    return WebHost
-        //        .CreateDefaultBuilder()
-        //        .UseStartup<TestStartup>();
-        //}
-
         protected override IHostBuilder CreateHostBuilder() =>
-            MicrosoftHost.CreateDefaultBuilder()
+            Host.CreateDefaultBuilder()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
