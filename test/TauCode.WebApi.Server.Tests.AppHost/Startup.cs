51dc2b3d3ca932a27878a57305d98b476b2a398d
Andrey Kovalenko <ak@taucode.com>
2023-04-07 20:06:51 +0300

    Updated from lab

--- test/TauCode.WebApi.Server.Tests.AppHost/Startup.cs
+++ test/TauCode.WebApi.Server.Tests.AppHost/Startup.cs
@@ -1,117 +1,119 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using FluentValidation;
-using Microsoft.OpenApi.Models;
-using TauCode.Cqrs.Autofac;
-using TauCode.Cqrs.Commands;
-using TauCode.Cqrs.Queries;
+//using Autofac;
+//using Autofac.Extensions.DependencyInjection;
+//using FluentValidation;
+//using Microsoft.OpenApi.Models;
+//using TauCode.Cqrs.Autofac;
+//using TauCode.Cqrs.Commands;
+//using TauCode.Cqrs.Queries;
 
-namespace TauCode.WebApi.Server.Tests.AppHost;
+// todo deal with this
 
-public class Startup : IAutofacStartup
-{
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+//namespace TauCode.WebApi.Server.Tests.AppHost;
 
-    public IConfiguration Configuration { get; }
+//public class Startup : IAutofacStartup
+//{
+//    public Startup(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
 
-    public ILifetimeScope AutofacContainer { get; private set; }
+//    public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        var coreAssembly = typeof(Startup).Assembly;
+//    public ILifetimeScope AutofacContainer { get; private set; }
 
-        services.AddControllers(options => options.Filters.Add(new ValidationFilterAttribute(coreAssembly)));
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc(
-                "v1",
-                new OpenApiInfo
-                {
-                    Title = "Demo Server RESTful Service",
-                    Version = "v1"
-                });
-            c.CustomSchemaIds(x => x.FullName);
-            c.EnableAnnotations();
-        });
-    }
+//    // This method gets called by the runtime. Use this method to add services to the container.
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        var coreAssembly = typeof(Startup).Assembly;
 
-    public void ConfigureContainer(ContainerBuilder containerBuilder)
-    {
-        var cqrsAssembly = typeof(Startup).Assembly;
+//        services.AddControllers(options => options.Filters.Add(new ValidationFilterAttribute(coreAssembly)));
+//        services.AddSwaggerGen(c =>
+//        {
+//            c.SwaggerDoc(
+//                "v1",
+//                new OpenApiInfo
+//                {
+//                    Title = "Demo Server RESTful Service",
+//                    Version = "v1"
+//                });
+//            c.CustomSchemaIds(x => x.FullName);
+//            c.EnableAnnotations();
+//        });
+//    }
 
-        // command dispatching
-        containerBuilder
-            .RegisterType<CommandDispatcher>()
-            .As<ICommandDispatcher>()
-            .InstancePerLifetimeScope();
+//    public void ConfigureContainer(ContainerBuilder containerBuilder)
+//    {
+//        var cqrsAssembly = typeof(Startup).Assembly;
 
-        containerBuilder
-            .RegisterType<AutofacCommandHandlerFactory>()
-            .As<ICommandHandlerFactory>()
-            .InstancePerLifetimeScope();
+//        // command dispatching
+//        containerBuilder
+//            .RegisterType<CommandDispatcher>()
+//            .As<ICommandDispatcher>()
+//            .InstancePerLifetimeScope();
 
-        // register API ICommandHandler decorator
-        containerBuilder
-            .RegisterAssemblyTypes(cqrsAssembly)
-            .Where(t => t.IsClosedTypeOf(typeof(ICommandHandler<>)))
-            .AsImplementedInterfaces()
-            .AsSelf()
-            .InstancePerLifetimeScope();
+//        containerBuilder
+//            .RegisterType<AutofacCommandHandlerFactory>()
+//            .As<ICommandHandlerFactory>()
+//            .InstancePerLifetimeScope();
 
-        // validators
-        containerBuilder
-            .RegisterAssemblyTypes(cqrsAssembly)
-            .Where(t => t.IsClosedTypeOf(typeof(AbstractValidator<>)))
-            .AsSelf()
-            .InstancePerLifetimeScope();
+//        // register API ICommandHandler decorator
+//        containerBuilder
+//            .RegisterAssemblyTypes(cqrsAssembly)
+//            .Where(t => t.IsClosedTypeOf(typeof(ICommandHandler<>)))
+//            .AsImplementedInterfaces()
+//            .AsSelf()
+//            .InstancePerLifetimeScope();
 
-        // query handling
-        containerBuilder
-            .RegisterType<QueryRunner>()
-            .As<IQueryRunner>()
-            .InstancePerLifetimeScope();
+//        // validators
+//        containerBuilder
+//            .RegisterAssemblyTypes(cqrsAssembly)
+//            .Where(t => t.IsClosedTypeOf(typeof(AbstractValidator<>)))
+//            .AsSelf()
+//            .InstancePerLifetimeScope();
 
-        containerBuilder
-            .RegisterType<AutofacQueryHandlerFactory>()
-            .As<IQueryHandlerFactory>()
-            .InstancePerLifetimeScope();
+//        // query handling
+//        containerBuilder
+//            .RegisterType<QueryRunner>()
+//            .As<IQueryRunner>()
+//            .InstancePerLifetimeScope();
 
-        containerBuilder
-            .RegisterAssemblyTypes(cqrsAssembly)
-            .Where(t => t.IsClosedTypeOf(typeof(IQueryHandler<>)))
-            .AsImplementedInterfaces()
-            .AsSelf()
-            .InstancePerLifetimeScope();
+//        containerBuilder
+//            .RegisterType<AutofacQueryHandlerFactory>()
+//            .As<IQueryHandlerFactory>()
+//            .InstancePerLifetimeScope();
 
-        containerBuilder
-            .RegisterInstance(this)
-            .As<IAutofacStartup>()
-            .SingleInstance();
-    }
+//        containerBuilder
+//            .RegisterAssemblyTypes(cqrsAssembly)
+//            .Where(t => t.IsClosedTypeOf(typeof(IQueryHandler<>)))
+//            .AsImplementedInterfaces()
+//            .AsSelf()
+//            .InstancePerLifetimeScope();
 
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-
-        this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
-
-        app.UseSwagger();
-        app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo Server RESTful Service"); });
+//        containerBuilder
+//            .RegisterInstance(this)
+//            .As<IAutofacStartup>()
+//            .SingleInstance();
+//    }
 
 
-        app.UseRouting();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
-}
+//    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseDeveloperExceptionPage();
+//        }
+
+//        this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+
+//        app.UseSwagger();
+//        app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo Server RESTful Service"); });
+
+
+//        app.UseRouting();
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapControllers();
+//        });
+//    }
+//}
